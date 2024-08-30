04fe0bbd60373dd88eeb3a77d4f0f06e158e8f38
Rafael <raffimartin069@gmail.com>
2024-08-31 02:56:14 +0800

    Add project files.

---
+++ DependecyInjection/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMVC.DependecyInjection
+{
+    // Mao ni TRUE POWER ni dotnet.
+    // Pinaagi ni Interface and Dependency Injection, pwede nimo i-configure ang imong app nga magamit ang lain-lain nga services nga gi-provide sa .NET Core.
+    // Mao ni ang pinaka-importante nga concept sa .NET Core.
+    // Kung wala ni, magka-problema ka sa pag-maintain sa imong app.
+    public class DependencyInjection
+    {
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlazorMVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
